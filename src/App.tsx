9de5c03217c6aa530618783a9a1a988b0df5b89d
temancoffeekupang01-jpg <temancoffeekupang01@gmail.com>
2026-09-07 14:42:28 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import HalamanKasir from './pages/HalamanKasir'
+
+function Home() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">Welcome ke Kasir Teman</h1>
+      <Link to="/kasir" className="mt-4 inline-block bg-orange-500 text-white px-6 py-3 rounded-lg">
+        Masuk ke Kasir
+      </Link>
+    </div>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Toaster richColors />
       <Routes>
-        <Route path="/" element={<h1>Welcome ke Kasir Teman</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/kasir" element={<HalamanKasir />} />
       </Routes>
     </BrowserRouter>
   )
